948788676cd76c970ab21ae8942d5a5cc79771a8
BadeBadeBade <icebade@gmail.com>
2017-05-15 23:25:57 +0800

    CRUD Supplier / Customer / Log-in System

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,54 +1,44 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-/*
-| -------------------------------------------------------------------------
-| URI ROUTING
-| -------------------------------------------------------------------------
-| This file lets you re-map URI requests to specific controller functions.
-|
-| Typically there is a one-to-one relationship between a URL string
-| and its corresponding controller class/method. The segments in a
-| URL normally follow this pattern:
-|
-|	example.com/class/method/id/
-|
-| In some instances, however, you may want to remap this relationship
-| so that a different class/function is called than the one
-| corresponding to the URL.
-|
-| Please see the user guide for complete details:
-|
-|	https://codeigniter.com/user_guide/general/routing.html
-|
-| -------------------------------------------------------------------------
-| RESERVED ROUTES
-| -------------------------------------------------------------------------
-|
-| There are three reserved routes:
-|
-|	$route['default_controller'] = 'welcome';
-|
-| This route indicates which controller class should be loaded if the
-| URI contains no data. In the above example, the "welcome" class
-| would be loaded.
-|
-|	$route['404_override'] = 'errors/page_missing';
-|
-| This route will tell the Router which controller/method to use if those
-| provided in the URL cannot be matched to a valid route.
-|
-|	$route['translate_uri_dashes'] = FALSE;
-|
-| This is not exactly a route, but allows you to automatically route
-| controller and method names that contain dashes. '-' isn't a valid
-| class or method name character, so it requires translation.
-| When you set this option to TRUE, it will replace ALL dashes in the
-| controller and method URI segments.
-|
-| Examples:	my-controller/index	-> my_controller/index
-|		my-controller/my-method	-> my_controller/my_method
-*/
-$route['default_controller'] = 'welcome';
+
+$route['default_controller'] = 'login_controller/index';
 $route['404_override'] = '';
-$route['translate_uri_dashes'] = FALSE;
+$route['translate_uri_dashes'] = TRUE;
+
+
+// $route['log-user'] = 'login_controller/login_validation';
+
+
+$route['dashboard'] = 'dashboard_controller/index';
+$route['user-logout'] = 'login_controller/logout';
+
+//************************************** CUSTOMER ROUTES
+//**************************************
+
+$route['customer-page'] = 'customer/customer_controller';
+
+$route['showlist-customer'] = 'customer/customer_controller/ajax_list';
+
+$route['edit-customer/(:num)'] = 'customer/customer_controller/ajax_edit/$1';
+
+$route['add-customer/(:num)'] = 'customer/customer_controller/ajax_add/$1';
+
+$route['update-customer/(:num)'] = 'customer/customer_controller/ajax_update/$1';
+
+$route['delete-customer/(:num)'] = 'customer/customer_controller/ajax_delete/$1';
+
+
+//************************************** SUPPLIER
+//**************************************
+$route['supplier-page'] = 'supplier/supplier_controller/index';
+
+$route['showlist-supplier'] = 'supplier/supplier_controller/ajax_list';
+
+$route['edit-supplier/(:num)'] = 'supplier/supplier_controller/ajax_edit/$1';
+
+$route['add-supplier/(:num)'] = 'supplier/supplier_controller/ajax_add/$1';
+
+$route['update-supplier/(:num)'] = 'supplier/supplier_controller/ajax_update/$1';
+
+$route['delete-supplier/(:num)'] = 'supplier/supplier_controller/ajax_delete/$1';
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to Dev-Pos</title>
+	<title>Welcome to CodeIgniter</title>
 
 	<style type="text/css">
 
@@ -68,10 +68,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <body>
 
 <div id="container">
-	<h1>Welcome to Dev-Pos!</h1>
+	<h1>Welcome to CodeIgniter!</h1>
 
 	<div id="body">
-		<p>The page you are zzzzz at is being generated dynamically by CodeIgniter.</p>
+		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
 
 		<p>If you would like to edit this page you'll find it located at:</p>
 		<code>application/views/welcome_message.php</code>
